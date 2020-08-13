5589e3cbf12a05cd5b0bd2e7911224810e5d0a86
lindyem <lindy.merwin@gmail.com>
2020-08-13 10:18:20 -0700

    Font Awesome

--- src/components/TabSection.jsx
+++ src/components/TabSection.jsx
@@ -28,7 +28,7 @@ class TabSection extends Component {
                       <ul>
                         <li>
                           <a href="#">
-                            Web and user interface design
+                            Web
                             <span> - Development</span>
                           </a>
                           html5/CSS, Javascript, ReactJS, NodeJS, MongoDB,
@@ -36,9 +36,44 @@ class TabSection extends Component {
                         </li>
                         <li>
                           <a href="#">
-                            User experience design <span> - UI/UX</span>
+                            User Experience Design <span> - UI/UX</span>
                           </a>
                           AdobeXD, Photoshop and Illustrator.
+                        </li>
+                      </ul>
+                      <div class="subheading mb-3">
+                        Programming Languages & Tools
+                      </div>
+                      <ul class="list-inline dev-icons">
+                        <li class="list-inline-item">
+                          <i class="fab fa-html5"></i>
+                        </li>
+                        <li class="list-inline-item">
+                          <i class="fab fa-css3-alt"></i>
+                        </li>
+                        <li class="list-inline-item">
+                          <i class="fab fa-js-square"></i>
+                        </li>
+
+                        <li class="list-inline-item">
+                          <i class="fab fa-react"></i>
+                        </li>
+                        <li class="list-inline-item">
+                          <i class="fab fa-node-js"></i>
+                        </li>
+                        <li class="list-inline-item">
+                          <i class="fab fa-sass"></i>
+                        </li>
+
+                        <li class="list-inline-item">
+                          <i class="fab fa-wordpress"></i>
+                        </li>
+
+                        <li class="list-inline-item">
+                          <i class="fab fa-npm"></i>
+                        </li>
+                        <li class="list-inline-item">
+                          <i class="fab fa-adobe"></i>
                         </li>
                       </ul>
                     </div>
@@ -60,7 +95,10 @@ class TabSection extends Component {
                           Classroom
                         </li>
                         <li>Yoga Specialty Certification</li>
-                        <li>Multi Subject Teaching Credential - California & Illinois</li>
+                        <li>
+                          Multi Subject Teaching Credential - California &
+                          Illinois
+                        </li>
                       </ul>
                     </div>
                   </TabPanel>
